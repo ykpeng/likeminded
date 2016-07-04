0505cb508558f8966a029f05358986203d3019ac
Yi-Ke Peng <yi.ke.peng@gmail.com>
2016-07-04 12:01:25 -0700

    edited config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -78,6 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.serve_static_files = true
-  # config.serve_static_assets = true
   config.log_level = :debug
 end
